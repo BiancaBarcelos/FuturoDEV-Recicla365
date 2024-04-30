d920fda7a66dd7307ca78b9f6784a998e7b75483
BiancaBarcelos <biancassbarcelos@gmail.com>
2024-04-29 21:52:10 -0300

    feat: criacao componente footer e estilizacao login e cadastro usuario

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+function Footer() {
+
+    return (
+        <div className="rodape">
+          <img src="./src/assets/logo_bianca.png" alt="" />
+          <div className="redesSociais">
+            <a href="https://github.com/BiancaBarcelos"><img ur src="./src/assets/git.png" alt="" /></a>
+            <a href="https://www.linkedin.com/in/bianca-barcelos-a98255114/"><img src="./src/assets/linkedin.png" alt="" /></a>
+            <a href="https://www.instagram.com/bisilvabarcelos/"><img src="./src/assets/instagram.png" alt="" /></a>
+          </div>
+        </div>
+    )
+}
+
+export default Footer;
